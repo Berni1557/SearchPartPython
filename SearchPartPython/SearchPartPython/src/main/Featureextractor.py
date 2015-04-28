a28c5c12ba49a52c81cd39704494f40de5c6c399
Berni1557 <SearchPart@yahoo.com>
2015-04-29 01:41:36 +0200

    Changed Featureextraction

--- SearchPartPython/SearchPartPython/src/main/Featureextractor.py
+++ SearchPartPython/SearchPartPython/src/main/Featureextractor.py
@@ -11,110 +11,81 @@ class Featureextracor(object):
     numSeg=None
     ks_pca=None
     numFreq=None
+    seg_threshold=None
     
-    def __init__(self,numHist,numSeg,MAX_COMPONENTS,ks_pca,numFreq):
+    def __init__(self,numHist,numSeg,MAX_COMPONENTS,ks_pca,numFreq,seg_threshold):
         self.numHist=numHist
         self.numSeg=numSeg
         self.MAX_COMPONENTS=MAX_COMPONENTS
         self.ks_pca=ks_pca
         self.numFreq=numFreq
+        self.seg_threshold=seg_threshold;
         
-    def generate_histogram(self,images,featurenum):
-        Nimages=list()
-        for Im in images:
-            h,s,v = cv2.split(Im)
+    def generate_histogram(self,mode,images,featurenum):
+        if mode=='predict':
             if featurenum is None:
-                bins = np.arange(0,261,self.numHist)
-                N1,_ = np.histogram(h,bins)
-                N2,_ = np.histogram(s,bins)
-                N3,_ = np.histogram(v,bins)
-                N=[N1,N2,N3]
-                return N
-            else:
-                bins = np.arange(0,261,self.numHist)
-                N=list()
-                for fn in featurenum:
-                    if fn<13:
-                        binsind=[bins[fn],bins[fn+1]-1]
-                        Ni,_ = np.histogram(h,binsind)
-                        N.append(Ni[0])
-                    elif fn>=13 and fn<26:
-                        binsind=[bins[fn-13],bins[fn-12]-1]
-                        Ni,_ = np.histogram(s,binsind)     
-                        N.append(Ni[0])  
-                    elif fn>=26 and fn<=38:
-                        binsind=[bins[fn-26],bins[fn-25]-1]
-                        Ni,_ = np.histogram(v,binsind)
-                        N.append(Ni[0])
-                    else:
-                        N.append(None)
-            Nimages.append(N)
-        return Nimages
-
-    def generate_segment(self,mode,images,featurenum):
-    
-        featuresAll=list()
-        num_x=self.numSeg[0] 
-        num_y=self.numSeg[1]
-        for Iin in images:
-            featuresIm=list()
-            I1=Iin.astype(double)
-            
-            #cv2.imshow('win1',Iin)
-            #cv2.waitKey()
-            
-            height, width, _ = Iin.shape
-            step_x=round((width/num_x)-0.5)
-            step_y=round((height/num_y)-0.5)
-            #Iin = cv2.imread("/home/bernifoellmer/Studium/SearchPartPython/SearchPartPython/SearchPartPython/SearchPartPython/data/T.JPG")
-            if featurenum is None:
-                for x in range(1, num_x):
-                    for y in range(1, num_y):
-                        sx=x*step_x
-                        sy=y*step_y
-                                        
-                        G=np.zeros((height,width),dtype=np.uint8)
-                        I2=G.astype(double)
-                        
-        
-                        threshold=30
-                        rgrowmod.rgrow_func(I1,I2,sx,sy,threshold)
-                        
-                        I1o = I1.astype(np.uint8)
-                        I2o = I2.astype(np.uint8)
-                        
-                        cv2.imshow('win1',I2o)
-                        cv2.waitKey()
-        
-                        _,thr = cv2.threshold(I2o, 127, 1, cv2.THRESH_BINARY)
-        
-                        contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        
-                        c=contours[0]
-                        rect = cv2.boundingRect(c)
-                        mean_val = cv2.mean(I1o,mask = thr)
-                        featuresSeg=[rect[0]+(rect[2]/2),rect[1]+(rect[3]/2),rect[2],rect[3],mean_val[0],mean_val[1],mean_val[2]];
-                        featuresIm.append(featuresSeg)
-                        
-                        #cv2.imshow('win1',thr)
-                        #cv2.waitKey()
-                        
-                featuresAll.append(featuresIm)
+                featuresAll=list()
+                for Im in images:
+                    h,s,v = cv2.split(Im)
+                    bins = np.linspace(0, 256, self.numHist+1)
+                    N1,_ = np.histogram(h,bins)
+                    N2,_ = np.histogram(s,bins)
+                    N3,_ = np.histogram(v,bins)
+                    N=np.concatenate((N1,N2,N3),axis=0)
+                    featuresAll.append(N)
                 return featuresAll
             else:
-                k=0
-                for x in range(1, num_x):
-                    for y in range(1, num_y):
-                        if (next((i for i in featurenum if i==k), None)):
-                            sx=x*step_x
-                            sy=y*step_y
+                featuresAll=list()
+                for Im in images:
+                    h,s,v = cv2.split(Im)
+                    bins = np.linspace(0, 256, self.numHist+1)
+                    N=[];
+                    b=len(bins)-1
+                    for fn in featurenum:
+                        if fn<b:
+                            binsind=[bins[fn],bins[fn+1]]
+                            Ni,_ = np.histogram(h,binsind)
+                            #N.append(Ni[0])
+                            #N=[N,Ni[0]]
+                            N=np.concatenate((N,Ni),axis=0)
+                        elif fn>=b and fn<b*2:
+                            binsind=[bins[fn-b],bins[fn-b+1]]
+                            Ni,_ = np.histogram(s,binsind)   
+                            #N=[N,Ni[0]]
+                            N=np.concatenate((N,Ni),axis=0)  
+                            #N.append(Ni[0])  
+                        elif fn>=b*2 and fn<3*b:
+                            binsind=[bins[fn-2*b],bins[fn-2*b+1]]
+                            Ni,_ = np.histogram(v,binsind)
+                            #N=[N,Ni[0]]
+                            N=np.concatenate((N,Ni),axis=0)
+                    featuresAll.append(N)
+                return featuresAll              
+
+    def generate_segment(self,mode,images,featurenum):
+        if mode=='predict':
+            if featurenum is None:
+                featuresAll=list()
+                num_x=self.numSeg 
+                num_y=self.numSeg
+                for Iin in images:
+                    featuresIm=list()
+                    I1=Iin.astype(double)
+                    height, width, _ = Iin.shape
+                    step_x=round((width/num_x)-0.5)
+                    step_y=round((height/num_y)-0.5)
+                    x_vec = np.linspace(round(step_x/2), width-round(step_x/2), num_x)
+                    y_vec = np.linspace(round(step_y/2), height-round(step_y/2), num_y)
+                    
+                    #Iin = cv2.imread("/home/bernifoellmer/Studium/SearchPartPython/SearchPartPython/SearchPartPython/SearchPartPython/data/T.JPG")
+                    featuresIm=[]
+                    for sx in x_vec:
+                        for sy in y_vec:
                                             
                             G=np.zeros((height,width),dtype=np.uint8)
                             I2=G.astype(double)
-                            
             
-                            threshold=30
-                            rgrowmod.rgrow_func(I1,I2,sx,sy,threshold)
+                            rgrowmod.rgrow_func(I1,I2,sx,sy,self.seg_threshold)
                             
                             I1o = I1.astype(np.uint8)
                             I2o = I2.astype(np.uint8)
@@ -126,14 +97,49 @@ class Featureextracor(object):
                             c=contours[0]
                             rect = cv2.boundingRect(c)
                             mean_val = cv2.mean(I1o,mask = thr)
+                            featuresSeg=[rect[0]+(rect[2]/2),rect[1]+(rect[3]/2),rect[2],rect[3],mean_val[0],mean_val[1],mean_val[2]];
+                            featuresIm=np.concatenate((featuresIm,featuresSeg),axis=0)
                             
-                            
-                            
-                            featuresSeg=[rect[0],rect[1],rect[2],rect[3],mean_val[0],mean_val[1],mean_val[2]];
-                            featuresIm.append(featuresSeg)
-                        k=k+1 
-                featuresAll.append(featuresIm)
-            return featuresAll
+                    featuresAll.append(featuresIm)
+                return featuresAll
+            else:
+                featuresAll=list()
+                num_x=self.numSeg 
+                num_y=self.numSeg
+                for Iin in images:
+                    featuresIm=list()
+                    I1=Iin.astype(double)
+                    height, width, _ = Iin.shape
+                    step_x=round((width/num_x)-0.5)
+                    step_y=round((height/num_y)-0.5)
+                    x_vec = np.linspace(round(step_x/2), width-round(step_x/2), num_x)
+                    y_vec = np.linspace(round(step_y/2), height-round(step_y/2), num_y)
+                    k=0
+                    for sx in x_vec:
+                        for sy in y_vec:
+                            if (next((i for i in featurenum if i==k), None)):
+                                                
+                                G=np.zeros((height,width),dtype=np.uint8)
+                                I2=G.astype(double)
+                                
+                                rgrowmod.rgrow_func(I1,I2,sx,sy,self.seg_threshold)
+                                
+                                I1o = I1.astype(np.uint8)
+                                I2o = I2.astype(np.uint8)
+                
+                                _,thr = cv2.threshold(I2o, 127, 1, cv2.THRESH_BINARY)
+                
+                                contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+                
+                                c=contours[0]
+                                rect = cv2.boundingRect(c)
+                                mean_val = cv2.mean(I1o,mask = thr)
+                                
+                                featuresSeg=[rect[0],rect[1],rect[2],rect[3],mean_val[0],mean_val[1],mean_val[2]];
+                                featuresIm=np.concatenate((featuresIm,featuresSeg),axis=0)
+                            k=k+1 
+                    featuresAll.append(featuresIm)
+                return featuresAll
 
     def generate_pca(self,mode,images):
         
@@ -184,7 +190,7 @@ class Featureextracor(object):
             err=np.sum(abs(matrix_train-back),axis=1)
             featuresAll=err      
             return featuresAll
-        else:
+        elif mode=='apriori':
             (s1,s2,_)=images[0].shape
             matrix_train=np.zeros((s1*s2,1),np.float32)  
             for Iin in images:
@@ -226,6 +232,9 @@ class Featureextracor(object):
             np.save(path+'eigenvectors.npy', eigenvectors)
             np.save(path+'eigenmean.npy', mean)
             return 1
+        else:
+            return 0
+        
         
     def generate_frequency(self,mode,images,featurenum):
         featuresAll=list()
@@ -279,9 +288,9 @@ Imn=cv2.imread('/home/bernifoellmer/Studium/SearchPartPython/SearchPartPython/Se
 #Im1=cv2.imread('/home/bernifoellmer/Studium/SearchPartPython/SearchPartPython/SearchPartPython/SearchPartPython/data/T.JPG')
 images=list()
 images.append(Im1)
-images.append(Im2)
-images.append(Im3)
-images.append(Im4)
+#images.append(Im2)
+#images.append(Im3)
+#images.append(Im4)
 #images.append(Im6)
 s1=100
 s2=50
@@ -290,10 +299,17 @@ for Iin in images:
     images[k]=cv2.resize(Iin, (s1, s2));
     k=k+1
 
-numHist=20;numSeg=5;MAX_COMPONENTS=50;ks_pca=11;numFreq=5;
-FG=Featureextracor(numHist,numSeg,MAX_COMPONENTS,ks_pca,numFreq)
-F=FG.generate_frequency(None, images,None)
-
+numHist=20;numSeg=5;MAX_COMPONENTS=50;ks_pca=11;numFreq=5;seg_threshold=30;
+FG=Featureextracor(numHist,numSeg,MAX_COMPONENTS,ks_pca,numFreq,seg_threshold)
+#F=FG.generate_frequency(None, images,None)
+#F=FG.generate_segment('predict', images, None)
+F1=FG.generate_segment('predict', images, [0])
+#F1=FG.generate_histogram('predict', images, [0])
+#F2=FG.generate_histogram('predict', images, [0,20])
+#print(F)
+print(F1)
+#print(F1)
+#print(F2)
 """
 start = time.time()
 for i in range(1):
