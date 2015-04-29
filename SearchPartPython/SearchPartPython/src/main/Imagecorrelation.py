486492e86f60c041fba5590e6781340f067a77d8
Berni1557 <SearchPart@yahoo.com>
2015-04-30 01:03:50 +0200

    Finish Featureextraction

---
+++ SearchPartPython/SearchPartPython/src/main/Imagecorrelation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Imagecorrelation
+
+
+# load component
+
+# create mean image
+
+# convert from rgb to hsv
+
+# convert image from rgb to hsv
+
+# correlation
+
+# filter position by threshold
+
+# compute correlation positions
+
+
+
+
+
+
